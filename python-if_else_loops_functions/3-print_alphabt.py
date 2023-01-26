aa4f8d870afcbf3a783e7804cf4a6128e61e61ea
luiluma <luma24lopez@gmail.com>
2023-01-26 16:56:42 -0500

    print alphabet except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
 for alpha in range(97, 123):
     if alpha in [101, 113]:
         continue
-    print("{:c}" .format(alpha), end="")
+    print("{:c}".format(alpha), end="")
