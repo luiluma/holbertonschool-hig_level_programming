ed80adbf427bd33cc3e5a52bdb4962e29f89373d
luiluma <luma24lopez@gmail.com>
2023-01-27 14:38:52 -0500

    uppercase error withspace

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for upper in range(len(str)):
         if str[upper] >= 'a' and str[upper] <= 'z':
             conv = str[upper]
-            conv = ord(conv) -32
+            conv = ord(conv) - 32
             conv = chr(conv)
             str = str[:upper] + conv + str[upper + 1:]
     print("{}".format(str))
