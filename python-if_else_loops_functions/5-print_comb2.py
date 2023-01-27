80c15e2642278ac14dbc215411a447e29df726c9
luiluma <luma24lopez@gmail.com>
2023-01-26 19:21:30 -0500

    print numbers ascending order from 0 - 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for list in range(0, 100):
     if list <= 98:
-        print(f'{list:02d}, '.format(list + 1), end="") #(end)list horizontally
+        print(f'{list:02d}, '.format(list + 1), end="")
+#(end)list horizontally
     if list > 98:
         print(list, end='\n')
