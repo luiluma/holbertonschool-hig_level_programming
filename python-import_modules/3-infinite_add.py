023f2db4e29e6371e2aacc172d75a0c99a38f166
luiluma <luma24lopez@gmail.com>
2023-01-27 22:02:14 -0500

    complety

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     len_args = len(args)
-    suma = 0
+    sum = 0
     for x in range(1, len_args):
-        suma += int(args[x])
-    print(suma)
+        sum += int(args[x])
+    print(sum)
