ade656549d66ed6027005670bb98800949ad06bb
luiluma <luma24lopez@gmail.com>
2023-01-27 21:42:19 -0500

    infinite add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import sys
 if __name__ == '__main__':
-    import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print(result)
+    args = sys.argv
+    len_args = len(args)
+    suma = 0
+    for x in range(1, len_args):
+        suma += int(args[x])
+    print(suma)
