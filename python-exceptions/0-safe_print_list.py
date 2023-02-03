72dc84ea049644de49bf9d5d95e40a1ee39e4731
luiluma <luma24lopez@gmail.com>
2023-02-03 18:09:18 -0500

    Change error output SAFE PRINT LIST

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    length = 0
-    for number in range(x):
-        try:
-            print("{}".format(my_list[number]), end='')
-            length += 1
-        except IndexError:
-            continue
-    print('')
-    return
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+        print()
+        return x
+    except (TypeError, IndexError):
+        print()
+        return i
