c2081c54342bc7dca6cf65a581f1374089f64876
luiluma <luma24lopez@gmail.com>
2023-02-07 12:00:39 -0500

    change error output empty class Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" This is an empty class named Square that defines a square """
-
+"""This is an empty class named Square that defines a square"""
 
 class Square:
-    "" empty class ""
+    """empty class"""
     pass
