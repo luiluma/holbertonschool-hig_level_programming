e2fc974527821d8968e32967c3a9b2e48f0f97c2
luiluma <luma24lopez@gmail.com>
2023-02-07 18:58:55 -0500

    that defines a square self, size=0

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square():
+    """class initialization"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise typeerror("size must be an integer")
+        if size < 0
+            raise valueerror("size must be >= 0")
