d8354a0795162acdd84e3548956f0fce3419aada
luiluma <luma24lopez@gmail.com>
2023-02-07 19:00:39 -0500

    that defines a square self, size=0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,6 +4,8 @@
 class Square():
     """class initialization"""
     def __init__(self, size=0):
+        """ Definition with private instance attribute size
+        which is assigned with the double underscore before given name"""
         self.__size = size
         if type(size) is not int:
             raise typeerror("size must be an integer")
