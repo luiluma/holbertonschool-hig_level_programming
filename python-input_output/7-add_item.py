e85cc11b226f71fdcdca84bc5d5f86d632afd74c
luiluma <luma24lopez@gmail.com>
2023-02-17 18:40:26 -0500

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-""" script that adds arguments to a python list and save them """
-import os.path
-from sys import argv
-""" sys allows me to receive the string to add in the file from the cl
-for accessing the filesystem os.path """
+"""
+    Script to save arguments to a file
+"""
 
+import sys
+import json
+from typing import List
+from os import path
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+if path.exists("add_item.json"):
+    items = load_from_json_file("add_item.json")
+else:
+    items = []
 
-p_list = []
-if os.path.exists("add_item.json"):
-    p_list = load_from_json_file("add_item.json")
-save_to_json_file(p_list + argv[1:], "add_item.json")
+items += sys.argv[1:]
+
+save_to_json_file(items, "add_item.json")
