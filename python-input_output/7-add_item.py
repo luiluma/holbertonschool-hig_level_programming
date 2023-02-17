07d5a995a17ca579af810d0422ad4a1980fa3824
luiluma <luma24lopez@gmail.com>
2023-02-17 18:42:05 -0500

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-    Script to save arguments to a file
-"""
-
-import sys
+'''Adds all arguments to a Python list,\
+     and then save them to a file'''
 import json
-from typing import List
-from os import path
-
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json"):
-    items = load_from_json_file("add_item.json")
-else:
-    items = []
+try:
+    arguments = load_from_json_file("add_item.json")
+except:
+    arguments = []
 
-items += sys.argv[1:]
-
-save_to_json_file(items, "add_item.json")
+for i in sys.argv[1:]:
+    arguments.append(i)
+save_to_json_file(arguments, "add_item.json")
