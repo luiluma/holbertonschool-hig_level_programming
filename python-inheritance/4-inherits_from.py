86fb8b93257417ceac3670e6a44fd33446f7ba64
luiluma <luma24lopez@gmail.com>
2023-02-16 20:44:19 -0500

    method definition returns true or flase

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+""" method to check inheritance """
 
 
 def inherits_from(obj, a_class):
-    """ Args:
-        returns True if the
-        object is an instance of a class that inherited (directly or indirectly) from the specified class;
-        otherwise False
-    """
+    """method definition returns True or False"""
     return isinstance(obj, a_class) and type(obj) is not a_class
