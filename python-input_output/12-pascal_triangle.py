227fa491fde6a0b0925300f232083333f57d989a
luiluma <luma24lopez@gmail.com>
2023-02-17 22:53:46 -0500

    error change pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-""" Pascal's Triangle method """
+"""
+    Pascal triangle module
+"""
 
 
 def pascal_triangle(n):
-    """ pascal triangle definition returns a list of lists of integers """
-    list_of_lists = []
-    row = []
-    prev_row = []
-
-    if n <= 0:
-        return []
-
-    for i in range(0, n + 1):
-        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j - 1] +
-            prev_row[j] or 1 for j in range(0, i)]
-        prev_row = row
-        list_of_lists += [row]
-    return list_of_lists[1:]
+    """
+        Returns a list of lists of integers
+        representing the Pascal’s triangle of n
+    """
+    ret = []
+    if (n <= 0):
+        return(ret)
+    elements = 1
+    element = [1]
+    while (elements <= n):
+        ret.append(element)
+        new_element = [1]
+        i = 0
+        while (i < (len(element) - 1)):
+            new_element.append(element[i] + element[i + 1])
+            i += 1
+        new_element.append(1)
+        element = new_element
+        elements += 1
+    return (ret)
