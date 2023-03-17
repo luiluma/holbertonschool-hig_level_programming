53a7f8ab815edd71c7f2733c88a737eb6a310a92
luiluma <luma24lopez@gmail.com>
2023-03-16 22:24:18 -0700

    force name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,9 @@
--- Read user
+-- script that creates the table force_name on your MySQL server
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- id and name
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE TABLE IF NOT EXISTS force_name(
 
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+	id INT
+	name VARCHAR(256)NOT NULL
+);
